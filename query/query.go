1d183a5ee8068833558d0e9388a2b9a217096d0b
Hantsaniala Eléo <hantsaniala@gmail.com>
2023-08-11 14:23:40 +0300

    Add create type query

--- query/query.go
+++ query/query.go
@@ -2,13 +2,14 @@ package query
 
 // Query represents a parsed query
 type Query struct {
-	Type       Type
-	TableName  string
-	Conditions []Condition
-	Updates    map[string]string
-	Inserts    [][]string
-	Fields     []string // Used for SELECT (i.e. SELECTed field names) and INSERT (INSERTEDed field names)
-	Aliases    map[string]string
+	Type         Type
+	TableName    string
+	Conditions   []Condition
+	CreateFields []Field
+	Updates      map[string]string
+	Inserts      [][]string
+	Fields       []string // Used for SELECT (i.e. SELECTed field names) and INSERT (INSERTEDed field names)
+	Aliases      map[string]string
 }
 
 // Type is the type of SQL query, e.g. SELECT/UPDATE
@@ -17,6 +18,8 @@ type Type int
 const (
 	// UnknownType is the zero value for a Type
 	UnknownType Type = iota
+	// Create represents a CREATE query
+	Create
 	// Select represents a SELECT query
 	Select
 	// Update represents an UPDATE query
@@ -30,6 +33,7 @@ const (
 // TypeString is a string slice with the names of all types in order
 var TypeString = []string{
 	"UnknownType",
+	"Create",
 	"Select",
 	"Update",
 	"Insert",
